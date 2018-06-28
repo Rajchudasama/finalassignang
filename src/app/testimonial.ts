75ac5f9cf4191709eb7f5cfef898f59dbd4444b9
raminder <raminderkaurmaan@yahoo.com>
2018-06-28 19:16:41 -0400

    testimonial class added

---
+++ src/app/testimonial.ts
@@ -0,0 +1,10 @@
+export class Testimonial {
+    id: number;
+    title: string;
+    content: string;
+    name: string;
+    company: string;
+    designation: string;
+    createdate: Date;
+    isActive: boolean;
+}
