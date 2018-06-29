b3777485f996162ba6180f31cb6c802b163bba74
raj <rajchudasam@outlook.com>
2018-06-28 22:14:49 -0400

    router configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
-import {FaqComponent} from './faq/faq.component'
+import {FaqComponent} from './faq/faq.component';
 
 const routes: Routes = [
-  {path: '',component: FaqComponent},
-  {path: 'faq',component: FaqComponent}
+  {path: '', component: FaqComponent},
+  {path: 'faq', component: FaqComponent}
 ];
 @NgModule({
-  exports: [RouterModule]
+  exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(routes)
+  ]
  /* imports: [
     CommonModule
   ],
